57469a791cba5baa7632d800ad821693be506e4d
henrique1232H <henriqueau111@gmail.com>
2026-08-10 17:54:48 -0300

    update card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,53 @@
+import { FaDice } from "react-icons/fa"
 import { api } from "../../service/api"
+import { useState } from "react"
+import { IoStarSharp } from "react-icons/io5"
 
 
-export default function Card() {
-    const searchFilm = async () => {
-        const response = await api.get("/movie/popular?language=pt-BR&page=2")
-       
+export default function Card({isActive, props, start, credits}) {
+    console.log(credits)
+    console.log(props)
 
-        console.log(response.data)
-    }
-    
-    searchFilm()
 
     return (
-        <div>
-            
+        <div class={"bg-surface mx-3 my-4 p-3 rounded-lg border-neutral-300 border-b font-sans"}>
+            {isActive ? (
+                <div class={"flex py-5 justify-center items-center flex-col font-medium"}>
+                  <img class={"cursor-pointer h-55"} src={`https://image.tmdb.org/t/p/w200${props.poster_path}`}/>
+
+                  <div class={"flex items-center justify-center gap-4 my-3"}>
+                    <span class={"bg-ink text-white text-sm px-2 py-1"}>{props.release_date[0]}{props.release_date[1]}{props.release_date[2]}{props.release_date[3]}</span>
+                    <span class={"flex items-center justify-center gap-1 text-accent bg-neutral-200 border-neutral-300 rounded-lg border-2  py-1 px-2 text-sm "}> <IoStarSharp/> {Math.round(props.vote_average)} / 10 </span>
+                    <span class={" border-neutral-300 border-2 text-gray-300 px-2"}>{props.runtime} min</span>
+                  </div>
+
+                  <h2 class={"italic text-lg"}> {props.title} </h2>
+                  <h3 class={"text-gray-300"}> {(credits.crew[0].name).toUpperCase()} </h3>
+
+                  <div class={"my-4 "}>
+                    {
+                      props.genres.map((genre) => {
+                        return <span key={genre.id} class={"text-xs border-gray-300 border-2 mr-2 py-1 px-3"}> {(genre.name).toUpperCase()} </span>
+                      })
+                    }
+                  </div>
+
+
+                </div>
+            ): (
+                <div class={"mt-6 py-5 flex justify-center items-center flex-col gap-5"}>
+                    <span class={"bg-ink flex justify-center items-center h-15 w-15 border-accent border-2"}>
+                        <FaDice fontSize={35} class={"text-accent"}/>
+                    </span>
+                    <h3 class={"italic text-2xl mt-3"}>Pronto para a Escolha Perfeita?</h3>
+                    <p class={"text-xs text-gray-700 font-sans font-medium w-80"}>Nenhum filme selecionado ainda. Ajuste seus filtros de gênero ou ator ao lado e clique no botão abaixo para girar a roleta.</p>
+
+                    <button onClick={start} class={"bg-ink flex items-center justify-center gap-2 px-6 py-3 border-accent border-2 text-white font-sans font-semibold cursor-pointer"}>
+                        <FaDice fontSize={20} class={"text-accent"}/>
+                        GIRAR ROLETA AGORA
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -5,8 +5,32 @@ import authenticantion from '../service/authenticantion'
 import Card from '../components/Card'
 export default function App() {
 
-  useEffect(() => {
+  const [runRollet, setRunRollet] = useState(false)
+  const [filmChoose, setFilmChoose] = useState([])
+  const [creditsForFilm, setCreditsForFilm] = useState([])
 
+    const searchFilm = async () => {
+      let filmsToRandom = []
+
+      for(let i = 1; i <= 5; i++) {
+        const response = await api.get(`/movie/popular?language=pt-BR&page=${i}`)
+        response.data.results.forEach(filmInArray => {
+          filmsToRandom = [...filmsToRandom, filmInArray];
+        })
+        i++
+      }
+      const random = Math.floor(Math.random() * (filmsToRandom.length - 0) + 0);
+
+      const filmCorrect = await api.get(`/movie/${filmsToRandom[random].id}?language=pt-BR`)
+      const credits = await api.get(`/movie/${filmsToRandom[random].id}/credits?language=pt-BR`);
+
+      setFilmChoose(filmCorrect.data)
+      setCreditsForFilm(credits.data)
+      setRunRollet(true)
+    }
+    
+    useEffect(() => {
+      
     const checkAuthentication = async () => {
 
     try {
@@ -24,11 +48,11 @@ export default function App() {
     <>
       <Header />
 
-      <div class={"mt-5 m-3"}>
+      <div class={"mt-10 m-3"}>
        <h2 class={"text-4xl text-ink italic"}>Sorteie o <span class={"text-accent"}>filme perfeito</span></h2>
-       <p class={"text-xs text-gray-700 font-medium mt-3"}>Defina gênero ou autor de preferência e deixe nossa roleta escolher o filme ideal para a sua noite.</p>
+       <p class={"text-xs text-gray-700 font-sans font-medium mt-3"}>Defina gênero ou autor de preferência e deixe nossa roleta escolher o filme ideal para a sua noite.</p>
 
-       <Card />
+       <Card isActive={runRollet} props={filmChoose} start={searchFilm} credits={creditsForFilm}/>
       </div>
     
     </>
